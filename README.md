e6531a8e6dd1b4089656a475c4d884c4ca1f2eb4
Benoit Daloze <eregontp@gmail.com>
2020-07-11 16:37:02 +0200

    Clean both draft and non-draft releases

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Keep last N releases
+
+Keeps the last N non-draft releases and the last N draft releases, and removes the rest.
--- dist/index.js
+++ dist/index.js
@@ -514,63 +514,81 @@ async function main() {
   }
 
   const dryRun = core.getInput('dry_run') === 'true'
-
   const lastTagFile = core.getInput('last_tag_file')
 
   const github = new GitHub(process.env.GITHUB_TOKEN)
   const { owner, repo } = context.repo
 
+  async function cleanupReleases(releases, kind) {
+    core.startGroup(`${kind} Releases`)
+
+    // Sort by older to newest
+    releases.sort((a, b) => Date.parse(releaseDate(a)) <= Date.parse(releaseDate(b)) ? -1 : 1)
+
+    console.log(`All ${releases.length} ${kind} releases (oldest to newest):`)
+    for (const release of releases) {
+      console.log(formatRelease(release))
+    }
+
+    let toDelete
+    if (releases.length <= n) {
+      console.log(`Only ${releases.length} ${kind} releases left (<= n=${n}), not deleting any`)
+      toDelete = []
+    } else {
+      toDelete = releases.slice(0, releases.length - n)
+    }
+
+    if (lastTagFile !== '') {
+      const lastTag = fs.readFileSync(lastTagFile, 'utf8').trim()
+      console.log(`Keeping release with tag ${lastTag}`)
+      toDelete = toDelete.filter(release => release.tag_name !== lastTag)
+    }
+
+    console.log(`Kept ${kind} releases:`)
+    for (const release of releases.filter(release => !toDelete.includes(release))) {
+      console.log(formatRelease(release))
+    }
+
+    console.log(`${toDelete.length} ${kind} releases to be deleted:`)
+    for (const release of toDelete) {
+      if (dryRun) {
+        console.log(`Would delete ${formatRelease(release)}`)
+      } else {
+        console.log(`\nDeleting ${formatRelease(release)}`)
+        await github.repos.deleteRelease({ owner, repo, release_id: release.id })
+
+        const tag = release.tag_name
+        const ref = `tags/${tag}`
+        console.log(`Deleting tag ${tag}`)
+        try {
+          await github.git.deleteRef({ owner, repo, ref })
+        } catch (e) {
+          console.log(`Tag ${tag} does not exist: ${e.message}`)
+        }
+      }
+    }
+    core.endGroup()
+  }
+
   let { data: releases } = await github.repos.listReleases({ owner, repo })
 
-  // Ignore draft releases
-  releases = releases.filter(release => !release.draft)
+  const draftReleases = releases.filter(release => release.draft)
+  const fullReleases = releases.filter(release => !release.draft)
 
-  // Sort by older to newest
-  releases.sort((a, b) => Date.parse(a.published_at) <= Date.parse(b.published_at) ? -1 : 1)
+  await cleanupReleases(fullReleases, "Full")
+  await cleanupReleases(draftReleases, "Draft")
+}
 
-  console.log(`All ${releases.length} releases (oldest to newest):`)
-  for (const release of releases) {
-    console.log(formatRelease(release))
-  }
-
-  let toDelete
-  if (releases.length <= n) {
-    console.log(`Only ${releases.length} releases left (<= n=${n}), not deleting any`)
-    toDelete = []
-  } else {
-    toDelete = releases.slice(0, releases.length - n)
-  }
-
-  if (lastTagFile !== '') {
-    const lastTag = fs.readFileSync(lastTagFile, 'utf8').trim()
-    console.log(`Keeping release with tag ${lastTag}`)
-    toDelete = toDelete.filter(release => release.tag_name !== lastTag)
-  }
-
-  console.log('Kept releases:')
-  for (const release of releases.filter(release => !toDelete.includes(release))) {
-    console.log(formatRelease(release))
-  }
-
-  console.log(`${toDelete.length} releases to be deleted:`)
-  for (const release of toDelete) {
-    if (dryRun) {
-      console.log(`Would delete ${formatRelease(release)}`)
-    } else {
-      console.log(`Deleting ${formatRelease(release)}`)
-      await github.repos.deleteRelease({ owner, repo, release_id: release.id })
-
-      const tag = release.tag_name
-      console.log(`Deleting tag ${tag}`)
-      await github.git.deleteRef({ owner, repo, ref: `tags/${tag}` })
-    }
-  }
-
-  console.log('Done')
+function releaseDate(release) {
+  return release.draft ? release.created_at : release.published_at
 }
 
 function formatRelease(release) {
-  return `Release published at ${release.published_at}, tag: ${release.tag_name}`
+  if (release.draft) {
+    return `Release created at ${release.created_at}, tag: ${release.tag_name}`
+  } else {
+    return `Release published at ${release.published_at}, tag: ${release.tag_name}`
+  }
 }
 
 async function run() {
